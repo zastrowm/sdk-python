2b1cf6bde77f701f9c6185651ea932e564607e09
Patrick Gray <pgrayy@amazon.com>
2026-01-07 10:35:47 -0500

    bidi - async - remove cancelling call (#1357)

--- tests/strands/experimental/bidi/_async/test_task_group.py
+++ tests/strands/experimental/bidi/_async/test_task_group.py
@@ -17,7 +17,7 @@ async def test_task_group__aexit__():
 
 
 @pytest.mark.asyncio
-async def test_task_group__aexit__exception():
+async def test_task_group__aexit__task_exception():
     wait_event = asyncio.Event()
     async def wait():
         await wait_event.wait()
@@ -35,7 +35,19 @@ async def test_task_group__aexit__exception():
 
 
 @pytest.mark.asyncio
-async def test_task_group__aexit__cancelled():
+async def test_task_group__aexit__task_cancelled():
+    async def wait():
+        asyncio.current_task().cancel()
+        await asyncio.sleep(0)
+
+    async with _TaskGroup() as task_group:
+        wait_task = task_group.create_task(wait())
+
+    assert wait_task.cancelled()
+
+
+@pytest.mark.asyncio
+async def test_task_group__aexit__context_cancelled():
     wait_event = asyncio.Event()
     async def wait():
         await wait_event.wait()
